6129dcfd546af5dec4d5f4b61d477c8c417f1466
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-08 00:04:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -27,4 +27,4 @@ end
 
 #emails = EmailParser.new("john@doe.com, person@somewhere.org, other@whatever.com bob@gmail.com person@somewhere.org")
 #puts emails.parse
-puts EmailParser.new("avi@test.com, avi@test.com").parse
+# puts EmailParser.new("avi@test.com, avi@test.com").parse
